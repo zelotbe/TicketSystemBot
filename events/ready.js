7675362a5abf80076b81e2328c880e5172585695
Quintin <zelotbe@gmail.com>
2022-06-17 02:03:20 +0200

    Clean Start

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = client => {
-  console.log(`${client.user.username} is online! Op ${client.guilds.cache.size} server(s)`);
-  client.user.setActivity("Tickets, please!", {type:"PLAYING"});
-}
--- events/reconnecting.js
+++
@@ -1,4 +0,0 @@
-module.exports = client => {
-  console.log(`Reconnecting at ${new Date()}`);
-
-};
